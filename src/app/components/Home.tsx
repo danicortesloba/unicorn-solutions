eec4b4db57fed63790fac5e5f6514bde644dffc8
Dani <dustyglass@gmail.com>
2023-12-01 13:41:59 -0300

    Header finished

---
+++ src/app/components/Home.tsx
@@ -0,0 +1,12 @@
+
+import styles from "./home.module.css"
+const Home = () => {
+    return (
+      <div className={styles.Box}>
+        
+         hello
+      </div>
+    );
+  };
+  
+  export default Home;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Header from "../app/components/Header"
+import Intro from "./components/Home"
 
 export default function Home() {
   return (
     <div>
       <Header/>
-      hello
+      <Intro/>
     </div>
   )
 }
