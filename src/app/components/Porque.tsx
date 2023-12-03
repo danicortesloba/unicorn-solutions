91e5e356964aba8170c27c96c1a29de5442a08f2
Dani <dustyglass@gmail.com>
2023-12-03 06:05:14 -0300

    fix styling

--- src/app/components/Porque.tsx
+++ src/app/components/Porque.tsx
@@ -8,16 +8,16 @@ const Porque = () => {
             <div className={styles.Box}>
             <div className={styles.Column}>
                 <ul>
-                    <li><FontAwesomeIcon icon={faStar}/> Amplia experiencia</li>
-                    <li><FontAwesomeIcon icon={faStar}/> Soluciones personalizadas</li>
-                    <li><FontAwesomeIcon icon={faStar}/> Atención personalizada</li>
+                    <li><FontAwesomeIcon icon={faStar} className={styles.Icon}/> Amplia experiencia</li>
+                    <li><FontAwesomeIcon icon={faStar} className={styles.Icon}/> Soluciones personalizadas</li>
+                    <li><FontAwesomeIcon icon={faStar} className={styles.Icon}/> Atención personalizada</li>
                 </ul>
             </div>
             <div className={styles.Column}>
                 <ul>
-                    <li><FontAwesomeIcon icon={faStar}/> Seguridad</li>
-                    <li><FontAwesomeIcon icon={faStar}/> Confidencialidad</li>
-                    <li><FontAwesomeIcon icon={faStar}/> Precios a tu alcance</li>
+                    <li><FontAwesomeIcon icon={faStar} className={styles.Icon}/> Seguridad</li>
+                    <li><FontAwesomeIcon icon={faStar} className={styles.Icon}/> Confidencialidad</li>
+                    <li><FontAwesomeIcon icon={faStar} className={styles.Icon}/> Precios a tu alcance</li>
                 </ul>
             </div>
         </div>
