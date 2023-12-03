2991b3588da30c251e460ec3a60ab1f33a18a126
Dani <dustyglass@gmail.com>
2023-12-02 23:26:07 -0300

    Favicon and email

--- src/app/components/Contacto.tsx
+++ src/app/components/Contacto.tsx
@@ -2,7 +2,7 @@ import styles from "./contacto.module.css"
 const Contacto = () => {
     return (
         <div className={styles.Box}>
-            <p>Email: unicornsolutionschile@gmail.com</p>
+            <p>Email: unicorn@unicornsolutions.cl</p>
             <p>Teléfonos: 	+56229736962 | +56979298644 </p>
             <p>Dirección: Antonio Bellet 193 Of. 302 - Providencia, RM, Chile
 </p>
