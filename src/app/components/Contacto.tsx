be117d96b4cb8a171fb3df96f97d8768299e2836
Dani <dustyglass@gmail.com>
2023-12-02 21:57:48 -0300

    contacto

---
+++ src/app/components/Contacto.tsx
@@ -0,0 +1,13 @@
+import styles from "./contacto.module.css"
+const Contacto = () => {
+    return (
+        <div className={styles.Box}>
+            <p>Email: unicornsolutionschile@gmail.com</p>
+            <p>Teléfonos: 	+56229736962 | +56979298644 </p>
+            <p>Dirección: Antonio Bellet 193 Of. 302 - Providencia, RM, Chile
+</p>
+        </div>
+    )
+}
+
+export default Contacto
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Intro from "./components/Home"
 import Servicios from "./components/Servicios"
 import Experiencia from "./components/Experiencia"
 import Porque from "./components/Porque"
+import Contacto from "./components/Contacto"
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <div id="servicios"><Servicios/></div>
       <div id="experiencia"><Experiencia/></div>
       <div id="porque"><Porque/></div>
+      <div id="contacto"><Contacto/></div>
     </div>
   )
 }
