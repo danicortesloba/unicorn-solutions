d2ecdc8a76542fe451a1d92b7610a2d47361eb8e
Dani <dustyglass@gmail.com>
2023-12-11 17:39:58 -0300

    Fix text

--- src/app/components/Experiencia.tsx
+++ src/app/components/Experiencia.tsx
@@ -4,7 +4,7 @@ const Experiencia = () => {
         <div className={styles.Box}>
             <h3>Tenemos experiencia desarrollando sitios web, aplicaciones 
                 y carros de compra para industrias tales como el retail, 
-                ecommerce, banca, cultura, salud, construcción y cultura.
+                ecommerce, banca, salud, construcción y cultura.
                 Agenda una reunión y descubramos lo que Unicorn Solutions puede
                 hacer por ti. </h3>
         </div>
